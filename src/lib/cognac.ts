7c0e8f57aa2fe2aec3aa9e112e0fa090bc88e8ab
Justin de Vesine <6026+ziz@users.noreply.github.com>
2024-10-08 16:03:28 -0700

    remove a few more unused tracking things

--- src/lib/cognac.ts
+++ src/lib/cognac.ts
@@ -1,14 +1,11 @@
 import { print, totalTurnsPlayed } from "kolmafia";
-import { $item, Session, get, set } from "libram";
+import { get, set } from "libram";
 
 import { LIFETIME_TURNS_SPENT, TURNS_SPENT } from "../prefs/properties";
 
-const initialSession = Session.current();
 const initialTurns = totalTurnsPlayed();
 
 export function save(): void {
-  const sessionDiff = Session.current().diff(initialSession);
-  const cognacs = sessionDiff.items.get($item`Ralph IX cognac`) ?? 0;
   set(TURNS_SPENT, get(TURNS_SPENT, 0) + totalTurnsPlayed() - initialTurns);
 
   set(LIFETIME_TURNS_SPENT, get(LIFETIME_TURNS_SPENT, 0) + totalTurnsPlayed() - initialTurns);
